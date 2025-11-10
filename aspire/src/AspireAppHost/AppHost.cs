6b42661b689e3832beea5ff86ff84f436ba21485
Tim Abell <t@0x5.uk>
2025-11-10 16:59:46 +0000

    Add regulator and account microservices

--- aspire/src/AspireAppHost/AppHost.cs
+++ aspire/src/AspireAppHost/AppHost.cs
@@ -12,4 +12,20 @@ builder
     .WithUrl("https://localhost:7154/")
     .WithExplicitStart();
 
+// todo: ports from here
+builder
+    .AddMicroservice(name: "regulator-service-facade", repoFolder: "epr-regulator-service-facade", workingDirectory: "src/EPR.RegulatorService.Facade.API")
+    .WithUrl("https://localhost:7253/")
+    .WithExplicitStart();
+
+builder
+    .AddMicroservice(name: "backend-account", repoFolder: "epr-backend-account-microservice", workingDirectory: "src/BackendAccountService.Api")
+    .WithUrl("http://localhost:5000/swagger/")
+    .WithExplicitStart();
+
+builder
+    .AddMicroservice(name: "frontend-account-creation", repoFolder: "epr-frontend-accountcreation-microservice", workingDirectory: "src/FrontendAccountCreation.Web/")
+    .WithUrl("https://localhost:7154/")
+    .WithExplicitStart();
+
 builder.Build().Run();
