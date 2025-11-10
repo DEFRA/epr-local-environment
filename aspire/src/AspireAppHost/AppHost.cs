5d79dac0558df63af06885e9991dd13d99162008
Tim Abell <t@0x5.uk>
2025-11-10 15:16:57 +0000

    Refactor - make re-usable AddMicroservice
    For easy adding of many microservices using the `dotnet run` method

--- aspire/src/AspireAppHost/AppHost.cs
+++ aspire/src/AspireAppHost/AppHost.cs
@@ -1,38 +1,10 @@
-internal class Program
-{
-    public static void Main(string[] args)
-    {
-        var builder = DistributedApplication.CreateBuilder(args);
+using EPR.AspireAppHost;
 
-        builder
-            .AddExecutable(name: "big-vibe-config-tool",
-                command: "dotnet",
-                workingDirectory: RepoPath("epr-tools-environment-variables"),
-                args: "run")
-            .WithUrl("http://localhost:5120/")
-            .WithExplicitStart();
+var builder = DistributedApplication.CreateBuilder(args);
 
-        builder.Build().Run();
-    }
+builder
+    .AddMicroservice(name: "big-vibe-config-tool", repoFolder: "epr-tools-environment-variables")
+    .WithUrl("http://localhost:5120/")
+    .WithExplicitStart();
 
-    /// <summary>
-    /// This assumes that you have all the configured microservices checked out in the same folder as this project.
-    /// Get the absolute path of a sibling folder (i.e. a microservice repo), optionally with a sub-path to add on.
-    /// </summary>
-    /// <param name="name">repo name</param>
-    /// <param name="subfolder">(optional) subfolder to include in returned path</param>
-    /// <returns>absolute path</returns>
-    static string RepoPath(string name, string? subfolder = null)
-    {
-        var repoRoot = Path.GetFullPath(
-            Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "..", "..")); // get back out of bin/debug etc all the way to parent directory
-
-        var path = Path.Combine(repoRoot, name);
-        if (!string.IsNullOrEmpty(subfolder))
-        {
-            return Path.Combine(path, subfolder);
-        }
-
-        return path;
-    }
-}
+builder.Build().Run();
---
+++ aspire/src/AspireAppHost/IResourceBuilderExtensions.cs
@@ -0,0 +1,13 @@
+namespace EPR.AspireAppHost;
+
+internal static class ResourceBuilderExtensions
+{
+    public static IResourceBuilder<ExecutableResource> AddMicroservice(
+        this IDistributedApplicationBuilder builder, string name, string repoFolder, string? workingDirectory = null)
+    {
+        return builder.AddExecutable(name: name,
+            command: "dotnet",
+            workingDirectory: PathFinder.RepoPath(repoFolder, workingDirectory),
+            args: "run");
+    }
+}
