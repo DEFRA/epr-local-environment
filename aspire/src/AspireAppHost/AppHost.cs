b8ea70857fdd05865f5cb9f5dfb5621ca4d67b5f
Nick Buckland <nick.buckland@smartitdev.com>
2025-11-19 11:51:23 +0000

    Add epr-pom-api-submission-status

--- aspire/src/AspireAppHost/AppHost.cs
+++ aspire/src/AspireAppHost/AppHost.cs
@@ -1,12 +1,22 @@
 using EPR.AspireAppHost;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 
 var builder = DistributedApplication.CreateBuilder(args);
+
+builder
+    .AddMicroservice("big-vibe-config-tool", "epr-tools-environment-variables")
+    .WithUrl("http://localhost:5120/");
+
 
 const string eprProducerRedisName = "epr-producer";
 
 var redis = builder.AddRedis(eprProducerRedisName)
     .WithPassword(null)
     .WithEndpoint(6379, 6379, name: "redis-tcp-endpoint", isProxied: false);
+
+const string accountsDbConnectionString =
+    "Server=127.0.0.1,1433;Initial Catalog=AccountsDb;User Id=sa;Password=Password1!;TrustServerCertificate=True;";
 
 const string password = "Password1!";
 var passwordParam = builder.AddParameter("sql-password", password);
@@ -17,12 +27,15 @@ var accountsDbSql = builder
     .WithEnvironment("ACCEPT_EULA", "Y")
     .WithEnvironment("MSSQL_SA_PASSWORD", password);
 
-const string accountsDbConnectionString =
-    "Server=127.0.0.1,1433;Initial Catalog=AccountsDb;User Id=sa;Password=Password1!;TrustServerCertificate=True;";
+builder
+    .AddMicroservice("common-data-api", "epr-common-data-api", "src/EPR.CommonDataService.Api")
+    .WithReference(redis)
+    .WithUrl("http://localhost:5001/");
 
 builder
-    .AddMicroservice("big-vibe-config-tool", "epr-tools-environment-variables")
-    .WithUrl("http://localhost:5120/");
+    .AddMicroservice("pom-api-submission-status", "epr-pom-api-submission-status", "src/EPR.SubmissionMicroservice.API")
+    .WithReference(redis)
+    .WithUrl("https://localhost:7206/");
 
 builder
     .AddMicroservice("regulator-frontend", "epr-regulator-service", "src/EPR.RegulatorService.Frontend.Web")
@@ -52,11 +65,6 @@ builder
     .WithReference(redis)
     .WithEnvironment("REDIS_INSTANCE_NAME", eprProducerRedisName)
     .WithUrl("https://localhost:7022/public-register");
-
-builder
-    .AddMicroservice("common-data-api", "epr-common-data-api", "src/EPR.CommonDataService.Api")
-    .WithReference(redis)
-    .WithUrl("http://localhost:5001/");
 
 builder
     .AddExecutable("likeC4",
