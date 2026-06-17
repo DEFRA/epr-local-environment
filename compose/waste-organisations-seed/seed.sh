a2c251c6bd164fa52105e701b84b57bf7b26172f
Tim Peel <tim.peel@equalexperts.com>
2026-06-17 12:59:13 +0100

    Introduce stubs and optional use, alongside updates to seeded organisations

--- compose/waste-organisations-seed/seed.sh
+++ compose/waste-organisations-seed/seed.sh
@@ -2,16 +2,26 @@
 
 AUTH=$(echo -n 'waste-organisations-seed:waste-organisations-seed-pwd' | base64)
 
-# User test+17122025143216@ee.com
-curl -v -X PUT \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: Basic $AUTH" \
-  -d @/payloads/94bfc917-b9b6-45d7-847b-e5f500bfe198.json \
-  http://waste-organisations:8080/organisations/94bfc917-b9b6-45d7-847b-e5f500bfe198
+put_organisation() {
+  PAYLOAD=$1
+  YEAR=$2
+  ORGANISATION_ID=$(basename "$PAYLOAD" .json)
 
-# User test+directproducer@ee.com
-curl -v -X PUT \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: Basic $AUTH" \
-  -d @/payloads/e2316c5e-d434-41da-8274-494dc0762d20.json \
-  http://waste-organisations:8080/organisations/e2316c5e-d434-41da-8274-494dc0762d20
+  sed "s/\"registrationYear\": 2025/\"registrationYear\": $YEAR/" "$PAYLOAD" | curl -v -X PUT \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Basic $AUTH" \
+    -d @- \
+    "http://waste-organisations:8080/organisations/$ORGANISATION_ID"
+}
+
+for YEAR in 2025 2026 2027 2028 2029 2030; do
+  # User test+17122025143216@ee.com
+  put_organisation \
+    /payloads/94bfc917-b9b6-45d7-847b-e5f500bfe198.json \
+    "$YEAR"
+
+  # User test+directproducer@ee.com
+  put_organisation \
+    /payloads/e2316c5e-d434-41da-8274-494dc0762d20.json \
+    "$YEAR"
+done
